fae062703f723a1d414576827463bb5fd9705488
syedzayn296-ops <syedzayn296@gmail.com>
2026-09-10 00:39:24 +0200

    Add smooth scrolling for anchor links

---
+++ script.js
@@ -0,0 +1,12 @@
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener("click", function (event) {
+    const target = document.querySelector(this.getAttribute("href"));
+
+    if (target) {
+      event.preventDefault();
+      target.scrollIntoView({
+        behavior: "smooth"
+      });
+    }
+  });
+});
